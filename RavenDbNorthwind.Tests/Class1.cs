1ee4c939b57525a4189997952d5ec4813f206fe8
unknown <allenf@DATPCDI032.digiterre.com>
2013-11-20 16:57:26 +0000

    adding test assembly

---
+++ RavenDbNorthwind.Tests/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Raven.Client.Document;
+using RavenDbNorthwind.Models.Db;
+
+namespace RavenDbNorthwind.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test, Explicit]
+        public void UpdateSupplierClrType()
+        {
+            var store = new DocumentStore {Url = "http://localhost:8080", DefaultDatabase = "Northwind"};
+            store.Initialize();
+            using (var session = store.OpenSession())
+            {
+                foreach (var supplier in session.Query<Supplier>().ToList())
+                {
+                    session.Advanced.GetMetadataFor(supplier);
+                }
+                session.SaveChanges();
+            }
+        }
+    }
+}
